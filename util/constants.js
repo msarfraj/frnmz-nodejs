77efb9b2050bce80dead526d6155a245a05d9ca8
Mohd Sarfraj <msarfraj@sapient.com>
2016-12-15 21:22:38 +0100

    testing git deployements

--- util/constants.js
+++ util/constants.js
@@ -8,7 +8,7 @@ exports.update_passwd_query = 'UPDATE user_data SET password = {password} WHERE 
 exports.update_views_query = 'UPDATE urls SET num_of_clicks = {VIEWS} WHERE id = {ID}';
 exports.insert_view = 'INSERT INTO stats SET ip = {IP}, url_id = {URL_ID}, referer = {REFERER}';
 exports.check_ip_query = 'SELECT COUNT(id) as counted FROM urls WHERE datetime_added >= now() - INTERVAL 1 HOUR AND ip = {IP}';
-//
+//herokudb
 /*exports.host = 'us-cdbr-iron-east-04.cleardb.net';//?reconnect=true';
 exports.user = 'b4fafab11ca4dc';
 exports.password = '182d894d';
