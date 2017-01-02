bf6520cb9f247bd3a1f3834588d2d344ada08f79
Mohd Sarfraj <msarfraj@sapient.com>
2017-01-02 10:52:34 +0100

    responsive top menu

--- public/media/js/appstyle.js
+++ public/media/js/appstyle.js
@@ -14,3 +14,12 @@ function setNavigation() {
         }
     });
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
